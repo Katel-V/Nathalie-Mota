a39fcbfd256837a00b90ad2c888047b5589d89af
= <katel.moiriat@gmail.com>
2024-11-22 19:18:18 +0100

    First commit

---
+++ wp-content/themes/nathaliemota/template-parts/modale.php
@@ -0,0 +1,7 @@
+<div id="modal" class="modal">
+    <div class="modal-content">
+        <img src="<?php echo get_template_directory_uri() . '/assets/images/contact.webp'; ?>" alt="contact">
+        <?php echo do_shortcode('[contact-form-7 id="686a218" title="contact"]'); ?>
+    </div>
+
+</div>
